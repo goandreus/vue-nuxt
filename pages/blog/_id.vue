24bcffaf964265285ca77d1d81e4d06a2dd76940
goandreus <andreshtml5@gmail.com>
2019-08-01 18:08:38 -0500

    async data

--- pages/blog/_id.vue
+++ pages/blog/_id.vue
@@ -3,7 +3,7 @@
         <div class="card">
             <div class="card-body">
                 <h1>{{articulos.title}}</h1>
-                <p class="small">{{articulos.userId}}</p>
+                <p class="small">{{articulos.nombreAutor}}</p>
                 <p>{{articulos.body}}</p>
                 <nuxt-link to="/blog" class="btn btn-primary">Atras</nuxt-link>
             </div>
@@ -18,17 +18,34 @@ export default {
 
     data(){
         return {
-            articulos: ''
+            
         }
     },
-    async created(){
-        try{
-        const res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${this.$route.params.id}`)
+    //async created(){
+    //    try{
+    //    const res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${this.$route.params.id}`)
+    //    this.articulos = res.data;
 
-        this.articulos = res.data;
+    //    const resAutor = await axios.get(`https://jsonplaceholder.typicode.com/users/${res.data.userId}`)
+    //    this.articulos.nombreAutor = resAutor.data.name;
+    //    }catch(error){
+    //        console.log(erorr);
+    //    }
+    //},
+
+    async asyncData({isDev, route, store, env, params, query, req, res, redirect, error}) {
+        try{
+        const res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
+        const articulos = res.data;
+
+        const resAutor = await axios.get(`https://jsonplaceholder.typicode.com/users/${res.data.userId}`)
+        articulos.nombreAutor = resAutor.data.name;
+
+        return {articulos};
         }catch(error){
             console.log(erorr);
+            return {error:error}
         }
-    }
+    },
 }
 </script>
