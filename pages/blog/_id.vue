80e5e95c36070de06e8ef4e92b72016874f60f8e
goandreus <andreshtml5@gmail.com>
2019-08-01 15:52:30 -0500

    posted API Id from blog

--- pages/blog/_id.vue
+++ pages/blog/_id.vue
@@ -1,5 +1,34 @@
 <template>
     <div class="container">
-        <h1>Detalle del articulo id: {{$route.params.id}}</h1>
+        <div class="card">
+            <div class="card-body">
+                <h1>{{articulos.title}}</h1>
+                <p class="small">{{articulos.userId}}</p>
+                <p>{{articulos.body}}</p>
+                <nuxt-link to="/blog" class="btn btn-primary">Atras</nuxt-link>
+            </div>
+        </div>
     </div>
 </template>
+
+<script>
+
+import axios from 'axios';
+export default {
+
+    data(){
+        return {
+            articulos: ''
+        }
+    },
+    async created(){
+        try{
+        const res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${this.$route.params.id}`)
+
+        this.articulos = res.data;
+        }catch(error){
+            console.log(erorr);
+        }
+    }
+}
+</script>
