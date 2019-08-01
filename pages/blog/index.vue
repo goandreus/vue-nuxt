0631f9f86076ae99c4bb227f8d13bad8b207cb52
goandreus <andreshtml5@gmail.com>
2019-08-01 15:21:39 -0500

    rest page dynamic for axios

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,26 +1,43 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-3" v-for="(item, index) in articulos" :key="index">
-                <h2>
-                    <nuxt-link :to="`/blog/${item.id}`">{{item.titulo}}</nuxt-link>
-                </h2>
+    <div class="container mt-5">
+        <div class="card my-2" v-for="(item, index) in articulos" :key="index">
+            <nuxt-link :to="`blog/${item.id}`">
+                <h1>{{item.title}}</h1>
+            </nuxt-link>
+            <p>{{item.body}}</p>
+            <div class="card-body">
+                <h1>Titulo</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium corporis neque eveniet laborum aliquid earum fugiat aperiam, nihil, quam provident, quisquam adipisci. Dicta, ut quod ipsum autem quisquam quas pariatur.
+                </p>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+
+import axios from 'axios';
+
 export default {
     data(){
         return{
             articulos:[
-                {id: 1,titulo: 'Titulo #1'},
-                {id: 2,titulo: 'Titulo #2'},
-                {id: 3,titulo: 'Titulo #3'},
-                {id: 4,titulo: 'Titulo #4'},
+               
 
             ]
+        }
+    },
+    
+    
+    async created(){
+
+        try{
+        const res = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+        console.log(res.data);
+        this.articulos = res.data
+        }catch(error){
+            console.log(error)
         }
     }
 }
