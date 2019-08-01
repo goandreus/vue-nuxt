0c2f04d795429f13aeeee456eb39ce90f96897ef
goandreus <andreshtml5@gmail.com>
2019-08-01 13:26:04 -0500

    nuxtlink

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,16 +1,28 @@
 <template>
     <div class="container">
-        <ul>
-            <li>articulo 1</li>
-            
-                        <li>articulo 1</li>
-
-            <li>articulo 1</li>
-
-            <li>articulo 1</li>
-
-            <li>articulo 1</li>
-
-        </ul>
+        <div class="row">
+            <div class="col-md-3" v-for="(item, index) in articulos" :key="index">
+                <h2>
+                    <nuxt-link :to="`/blog/${item.id}`">{{item.titulo}}</nuxt-link>
+                </h2>
+            </div>
+        </div>
     </div>
 </template>
+
+<script>
+export default {
+    data(){
+        return{
+            articulos:[
+                {id: 1,titulo: 'Titulo #1'},
+                {id: 2,titulo: 'Titulo #2'},
+                {id: 3,titulo: 'Titulo #3'},
+                {id: 4,titulo: 'Titulo #4'},
+
+            ]
+        }
+    }
+}
+</script>
+
